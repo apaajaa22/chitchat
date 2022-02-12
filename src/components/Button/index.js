e9b82ee191b88b22ed81e850e9b1924dc86aaa8e
rahadian reza r <reza.rahadian1@gmail.com>
2022-02-12 18:27:23 +0700

    smooth trasition button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ function Button({ onClick, label }) {
   return (
     <button
       onClick={onClick}
-      className="bg-button text-white w-72 h-12 rounded-md hover:scale-105 transform-gpu"
+      className="bg-button text-white w-72 h-12 rounded-md hover:scale-105 transform-gpu duration-300"
     >
       {label}
     </button>
